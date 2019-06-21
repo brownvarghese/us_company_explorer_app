e320f32fb9a8e16681bcd6b394232b9618909b0e
Radha Mahalingam <rmahalin@Radhas-MBP.fios-router.home>
2019-06-21 06:13:45 -0400

    completed version of the app

---
+++ App Project Folder/app.py
@@ -0,0 +1,181 @@
+# US Company Exploration App Project 
+
+# Developed by Radha Mahalingam on 6/18/19
+
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/USCA_DATABASE.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+USCA_Fulldata = Base.classes.USCA_TABLE
+uscctab = Base.classes.US_COMPANY_CATEGORY_TABLE
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+@app.route("/usccnames")
+def usccnames():
+    """Return a list of US Company Category names."""
+
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(uscctab).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    # print(df)
+    df1=df.set_index('company_category').T
+    # print(df1)
+    # Return a list of the column names (sample names)
+    return jsonify(list(df1.columns)[0:])
+
+@app.route("/seldata/<cccat>")
+def cccat_seldata(cccat):
+    """Return the data for a given company category."""
+
+    sel = [
+        USCA_Fulldata.company_name,
+        USCA_Fulldata.company_category,
+        USCA_Fulldata.state,
+        USCA_Fulldata.full_time_employees,
+        USCA_Fulldata.business_model,
+        USCA_Fulldata.agency_name
+    ]
+
+    results = db.session.query(*sel).filter(USCA_Fulldata.company_category == cccat).all()
+    print(results)
+
+    # Create a dictionary entry for each row of metadata information
+    cccat_metadata = []
+    sno = 0
+
+    for result in results:
+        cccat_dict = {}
+        sno += 1
+        cccat_dict["sno"] = sno
+        cccat_dict["company_name"] = result[0]
+        cccat_dict["company_category"] = result[1]
+        cccat_dict["state"] = result[2]
+        cccat_dict["full_time_employees"] = result[3]
+        cccat_dict["business_model"] = result[4]
+        cccat_dict["agency_name"] = result[5]
+        cccat_metadata.append(cccat_dict)
+
+    print(cccat_metadata)
+    return jsonify(cccat_metadata)
+
+@app.route("/mapdata/<cccat>")
+def cccat_mapdata(cccat):
+
+    mquery = db.session.query(USCA_Fulldata.state, func.count(USCA_Fulldata.company_name.distinct())).\
+    filter(USCA_Fulldata.company_category == cccat).group_by(USCA_Fulldata.state).all()
+    
+    # print(mquery)        
+
+    cccat_mdata = []
+
+    for s in mquery:
+        m_dict = {}
+        m_dict["id"]="US."+s[0]
+        m_dict["value"]=s[1]
+        cccat_mdata.append(m_dict)
+
+    return jsonify(cccat_mdata)
+
+@app.route("/piedata/<cccat>")
+def cccat_piedata(cccat):
+
+    pquery = db.session.query(USCA_Fulldata.full_time_employees, USCA_Fulldata.company_name).filter(USCA_Fulldata.company_category == cccat).order_by(USCA_Fulldata.company_name).all()
+    
+    # print(pquery)        
+
+    c1=0
+    c2=0
+    c3=0
+    c4=0
+    c5=0
+    old_com = ""
+
+    for s in pquery:
+
+        if old_com != s[1]:
+           old_com = s[1]
+           if s[0] < 501:
+              c1 = c1 + 1
+           elif s[0] < 2501:
+              c2 = c2 + 1
+           elif s[0] < 5001:
+              c3 = c3 + 1
+           elif s[0] < 10001:
+            c4 = c4 + 1
+           else:
+            c5 = c5 + 1 
+
+    cccat_pdata = []
+
+    p_dict={}
+    p_dict["FTE_range"]='- 500'
+    p_dict["range_count"]=c1
+    cccat_pdata.append(p_dict)
+    p_dict={}
+    p_dict["FTE_range"]='501-2500'
+    p_dict["range_count"]=c2
+    cccat_pdata.append(p_dict)
+    p_dict={}
+    p_dict["FTE_range"]='2501-5000'
+    p_dict["range_count"]=c3
+    cccat_pdata.append(p_dict)
+    p_dict={}
+    p_dict["FTE_range"]='5001-10000'
+    p_dict["range_count"]=c4
+    cccat_pdata.append(p_dict)
+    p_dict={}
+    p_dict["FTE_range"]='10001 - '
+    p_dict["range_count"]=c5
+    cccat_pdata.append(p_dict)
+
+    return jsonify(cccat_pdata)
+
+@app.route("/bardata/<cccat>")
+def cccat_bardata(cccat):
+
+    bquery = db.session.query(USCA_Fulldata.business_model,func.count(USCA_Fulldata.company_name.distinct())).\
+             filter(USCA_Fulldata.company_category == cccat).group_by(USCA_Fulldata.business_model).all()
+    
+    # print(bquery)        
+
+    cccat_bdata = []
+
+    for s in bquery:
+        b_dict = {}
+        b_dict["Bus_Model"]=s[0]
+        b_dict["Company_count"]=s[1]
+        cccat_bdata.append(b_dict)
+
+    return jsonify(cccat_bdata)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ App Project Folder/static/js/app.js
@@ -0,0 +1,249 @@
+// US Company Exploration App Project
+
+var cat_selected;
+
+function buildCharts() {
+
+  $('#chartmap').empty();
+
+  document.getElementById("tbldata").style.display="table";
+  document.getElementById("heading1").style.display="block";
+  document.getElementById("heading2").style.display="block";
+  document.getElementById("heading3").style.display="block";
+
+  // Use the list of sample names to populate the select options
+  var url = `/piedata/${cat_selected}`;
+
+  console.log("Pie URL : " + url)
+
+  d3.json(url).then(function (res){
+    // if (error) return console.log(error);
+
+    console.log(res)
+
+    var arr = [];
+
+    arr.push([res[0].FTE_range, res[0].range_count]);
+    arr.push([res[1].FTE_range, res[1].range_count]);
+    arr.push([res[2].FTE_range, res[2].range_count]);
+    arr.push([res[3].FTE_range, res[3].range_count]);
+    arr.push([res[4].FTE_range, res[4].range_count]);
+
+    console.log(arr)
+
+    var chart = c3.generate({
+      bindto: '#chartpie',
+      data: {
+        columns: arr,
+        type : 'pie',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); },
+		    onrendered: function () {$("#chartmap").append("<h6>Company Count by State</h6>"); }
+      },
+      pie: {
+        label: {
+            format: function (value, ratio, id) {
+                return d3.format('')(value);
+            }
+        }
+    }
+  }) 
+  setTimeout(function () {
+    chart.transform('pie');
+  }, 1000);
+
+  setTimeout(function () {
+    chart.transform('bar');
+  }, 2000);
+
+  setTimeout(function () {
+    chart.transform('pie');
+  }, 3000);  
+
+  });
+
+// Render US Map
+
+  var url = `/mapdata/${cat_selected}`;
+
+  console.log("Map URL : " + url)
+
+  d3.json(url).then(function (res){
+    // if (error) return console.log(error);
+
+  console.log(res)
+// map display for the company count by US state using anychart-map.js
+  anychart.onDocumentReady(function() {
+    // create map
+      var map = anychart.map();
+  
+    // create data set
+      var dataSet = anychart.data.set(res);
+
+    // create choropleth series
+      series = map.choropleth(dataSet);
+  
+    // set geoIdField to 'id', this field contains in geo data meta properties
+      series.geoIdField('id');
+  
+// making of the ordinal colorRange
+
+      ocs = anychart.scales.ordinalColor([
+      {less: 1},
+      {from: 1, to: 6},
+      {from: 6, to: 11},
+      {from: 11, to: 21},
+      {greater:21}
+      ]);        
+  
+      ocs.colors(['rgb(253,225,86)','rgb(248,196,57)', 'rgb(244,168,19)', 'rgb(198,109,1)', 'rgb(152,58,0)']);
+
+      series.colorScale(ocs);
+
+      map.geoData(anychart.maps['united_states_of_america']);
+
+      // enable the tooltips and format them at once
+      // series.tooltip().format(function(e){
+      //   return "Capital: " + e.getData("capital") +"\n"+
+      //   "Population: " + e.getData("population")
+      // });
+
+      // stroke the undefined regions
+      map.unboundRegions().stroke('green');
+
+      // set the fill for the regions that haven't defined in the dataSet
+      map.unboundRegions().fill('#eee');
+  
+    //set map container id (div)
+      map.container('chartmap');
+  
+    //initiate map drawing
+      map.draw();
+   
+      map.zoomIn();
+    })
+  });
+
+  var url = `/bardata/${cat_selected}`;
+
+  console.log("Bar URL : " + url)
+
+  d3.json(url).then(function (res1){
+    // if (error) return console.log(error);
+
+  console.log(res1)
+
+  var arr1 = [];
+
+  for (var i in res1) {
+    arr1.push([res1[i].Bus_Model, res1[i].Company_count])
+  }
+
+  console.log(arr1)
+
+  var chart2 = c3.generate({
+    bindto: '#chartbar',
+
+    data: {
+      columns: arr1,
+      type: 'bar',
+      // labels: false
+    },
+    bar: {
+      width: {
+          ratio: 0.8 // this makes bar width 50% of length between ticks
+      }
+      // or
+      //width: 100 // this makes bar width 100px
+    },
+    axis: {
+      x: {
+        label: {
+        text: 'Business Model Type',
+        position: 'outer-center'
+        }
+      }
+    },
+    axis: {
+     y: {
+        label: {
+        text: 'No of US Companies',
+        position: 'outer-middle'
+        }
+     }  
+    },
+  })    	
+  });
+
+  var $tbody = document.querySelector('tbody');
+  var url = `/seldata/${cat_selected}`;
+  
+  console.log("Table URL : " + url)
+  
+  d3.json(url).then(function (res2){
+  
+    // if (error) return console.log(error);
+  
+    console.log(res2)
+  
+    var arr2 = [];
+  
+    for (var i in res2) {
+      arr2.push([res2[i].sno, res2[i].company_name, res2[i].company_category, res2[i].state, res2[i].full_time_employees, res2[i].business_model, res2[i].agency_name])
+    }
+  
+    console.log(arr2)
+  
+    $tbody.innerHTML = '';
+
+    console.log("Entering FOR loop");
+
+    for (var i = 0; i < arr2.length; i++) {
+      // Get get the current address object and its fields
+        console.log("First for loop")
+        var address = arr2[i];
+        var fields = Object.keys(address);
+      // Create a new row in the tbody, set the index to be i + startingIndex
+        var $row = $tbody.insertRow(i);
+        for (var j = 0; j < fields.length; j++) {
+          console.log("Second for loop")
+        // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+          var field = fields[j];
+          var $cell = $row.insertCell(j);
+          $cell.innerText = address[field];
+        }
+    }
+  });
+}
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selUSCC");
+
+  console.log(selector)
+
+  // Use the list of sample names to populate the select options
+  d3.json("/usccnames").then((USCCNames) => {
+    USCCNames.forEach((cname) => {
+      selector
+        .append("option")
+        .text(cname)
+        .property("value", cname);
+    });
+
+    // Use the first sample from the list to build the initial plots
+
+    cat_selected = USCCNames[0];
+    // buildCharts();
+  });
+};
+
+function selectedcategory() {
+cat_selected=""; 
+cat_selected=$("#selUSCC").val();  
+console.log("I am here " + cat_selected);
+};
+
+// Initialize the dashboard
+init();
